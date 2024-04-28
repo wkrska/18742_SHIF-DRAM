c864c1b84cc1fcfa82bcf081e26f613c1b1e2141
William Krska <wkrska@andrew.cmu.edu>
2024-04-28 10:18:42 -0400

    debugging

--- run_test.sh
+++ run_test.sh
@@ -2,8 +2,8 @@ filename="my_test"
 # filename="bitweave-buddy"
 
 cd microworkloads/
-gcc -static -std=c99 -O3 -msse2 -I ../gem5/util/m5 m5op_x86.S rowop.S** "$filename".c -o "$filename".exe
+gcc -DDEBUG -DSIZE=512 -static -std=c99 -O3 -msse2 -I ../gem5/util/m5 m5op_x86.S rowop.S** "$filename".c -o "$filename".exe
 cd .. 
 cd gem5/
-./build/X86/gem5.opt configs/example/se.py --cpu-type=detailed --caches --l2cache --mem-type=DDR4_2400_x64 --mem-size=8192MB -c /home/18872_SHIF-DRAM/microworkloads/"$filename".exe -o "10 1"
+./build/X86/gem5.opt configs/example/se.py --cpu-type=detailed --caches --l2cache --mem-type=DDR4_2400_x64 --mem-size=8192MB -c /home/18872_SHIF-DRAM/microworkloads/"$filename".exe -o "10 1" > ../test.txt
 cd ..
